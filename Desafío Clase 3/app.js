5e5ecad4d8c29c2b82d992e6ec533f3475a9fbcc
PabloNahabedian <89050566+PabloNahabedian@users.noreply.github.com>
2021-09-27 10:07:15 -0300

    Desafio clase 3

---
+++ Desafío Clase 3/app.js
@@ -0,0 +1,29 @@
+/*
+Ejemplo 1
+
+let i = Number(prompt("Ingresar un numero"))
+
+for (i; i <= 5; i++) {
+    alert(i)
+}
+*/
+
+/*
+Ejemplo 2
+
+let i = prompt("Ingresar tu pass")
+
+while (i != "ESC") {
+    alert("La pass ingresada es incorrecta");
+    i = prompt("Ingresa otro dato");
+}
+*/
+
+
+/*
+let numero = Number(prompt("Ingresar un numero"))
+
+for (let i = 0; i < numero; i++) {
+    alert("Hola")
+}
+*/
